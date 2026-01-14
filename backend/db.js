672920ba98241126568e24a7327424c011c0b787
Tristr4m <rogerssantana3002@gmail.com>
2026-01-14 01:06:29 -0500

    v1

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,12 @@
 import pg from "pg";
 
 export const pool = new pg.Pool({
+    /*
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
+    */
+   db: DB_URL
 });
